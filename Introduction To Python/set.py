6fbc3ef211bf341d1a899b902d867ceef82df89a
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2024-10-13 23:07:23 +0300

    Update set.py

--- Introduction To Python/set.py
+++ Introduction To Python/set.py
@@ -1,1 +1,17 @@
+#set - no indexing, unordered, non-duplicated
+
+fv_nums = {4,3,2, 'as'}
+
+fv_nums.add(True)
+
+print(fv_nums)
+
+#{} - empty dict!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+
+my_list = [1,23,2,2,4,4]
+
+print(set(my_list))
+
+# |, & , -, ^, < - Andi stuff
+
 
