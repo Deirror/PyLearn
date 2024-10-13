d72ddd94f0b8f0b628abbd7069a934598c9ac1d8
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2024-10-13 22:15:30 +0300

    Create first_file.py

---
+++ Introduction To Python/first_file.py
@@ -0,0 +1,29 @@
+#help is your friend
+
+#print(help(str))
+#print(type(type))
+
+#+,-,*,/,//,**,%
+
+#common datat types : int, float, complex, str, bool, None,
+
+# 'T'rue, 'F'alse
+
+#everything here is an object...
+
+list =[1, 4] # list = [], list = list()
+tuple = (1, 2, 3)
+set = {1, 2, 3, 4}
+dictK = {"SCHEISE":69, "PAPUUREN":96 }
+
+a, *b, c = 1, 2, 3, 4 #!!!!!!
+
+(s, d) = 1, 2 # Brackets don't matter
+
+dict_second = dict(fr="Paris", it="Italy")
+dict_second = dict.fromkeys([1, 2, 3], 'Unknown')
+dict_second = dict([('One', 'I'), ('Two', 'II')])
+
+print(dict_second)
+
+#more info on the other .py files (probably for array types)
