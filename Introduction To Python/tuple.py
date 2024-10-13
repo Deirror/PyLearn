6dba9369b6a5438ec1fa81d022ba771df88e3df2
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2024-10-13 23:00:13 +0300

    Update tuple.py

--- Introduction To Python/tuple.py
+++ Introduction To Python/tuple.py
@@ -1,1 +1,14 @@
+#tuple - immutable, ordered as list
 
+#has methods - index, count, sort...
+
+args = ([1,'a']) # not a tuple
+args = (2, args) # tuple OK
+
+print(args)
+
+# Immutable са числата, низовете, tuple-ите, True, False, None etc.
+
+args[1][1] = 's'
+
+print(args[1])
