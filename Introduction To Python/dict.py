d5deb62d469daf9b0db6ba87811820e85e4b4c03
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2024-10-13 23:12:01 +0300

    Update dict.py

--- Introduction To Python/dict.py
+++ Introduction To Python/dict.py
@@ -1,1 +1,10 @@
 
+#dict - set, but has key(index) to access a value
+
+my_dict = {"IT" : 1, 'USA': 66, 'GR':44}
+
+print('Eric\'s last names is ' + str(my_dict['IT']))
+
+my_dict['FR'] = -10000000
+
+print(my_dict.get('UK','OMG no')) # error, don't have throw excpts
