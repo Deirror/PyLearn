23d019b65a43c3f64990776c73461aa42b6608f9
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2024-10-20 15:40:18 +0300

    Update main.py

--- tasks/randomly found/Seconds To Time/main.py
+++ tasks/randomly found/Seconds To Time/main.py
@@ -1,1 +1,21 @@
 
+SECONDS_IN_HOUR = 3600
+SECONDS_IN_MINUTE = 60
+
+HOURS_IN_DAY = 24
+MINUTES_IN_HOUR = 60
+SECONDS_IN_MINUTE = 60
+
+def seconds_to_time(seconds: int) -> tuple:
+    return ((seconds // SECONDS_IN_HOUR) % HOURS_IN_DAY,
+                 (seconds //SECONDS_IN_MINUTE) % MINUTES_IN_HOUR,
+                  seconds % SECONDS_IN_MINUTE)
+
+def main() -> None:
+    seconds = input("Enter seconds: ")
+    if not seconds.isdigit():
+        return None
+    print(seconds_to_time(int(seconds)))
+
+if __name__ == '__main__':
+    main()
