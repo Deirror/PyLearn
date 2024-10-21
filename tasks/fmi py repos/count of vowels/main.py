1b823fce0ecd9b79fd99f9a792e7eed011eb1ca8
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2024-10-21 15:50:24 +0300

    Update main.py

--- tasks/fmi py repos/count of vowels/main.py
+++ tasks/fmi py repos/count of vowels/main.py
@@ -1,9 +1,20 @@
-def number_of_vowels(text: str) -> int:
+def number_of_vowels_v1(text):
     return (text.count('a') +
             text.count('e') +
             text.count('i') +
             text.count('o') +
             text.count('u'))
+    
+VOWELS = 'aeiou'
+def number_of_vowels_v2(text):
+    count = 0
+    for ch in text:
+        if ch in VOWELS:
+            count += 1
+    return count
+    
+def number_of_vowels_v3(text):
+    return sum(1 for ch in text if ch.lower() in VOWELS)
 
 if __name__ == '__main__':
-    print(number_of_vowels("The quick brown fox jumps over the lazy dog."))
+    print(number_of_vowels_v1("The quick brown fox jumps over the lazy dog."))
