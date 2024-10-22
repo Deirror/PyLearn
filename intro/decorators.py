7d43849a9893bdba1fdc25350c6b235feaf2f47e
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2024-10-22 21:14:51 +0300

    Update decorators.py

--- intro/decorators.py
+++ intro/decorators.py
@@ -1,1 +1,24 @@
-#none so far
+#1
+def start(x):
+    def increment(y):
+        return x + y
+    return increment
+
+#2
+def my_start(func):
+    def wrapper(x, y):
+        return func(x, y)
+    return wrapper
+
+@my_start
+def my_increment(x, y):
+    return x + y
+
+first_inc = start(0)
+second_inc = start(8)
+
+print(first_inc(3))
+print(second_inc(3))
+
+print(my_increment(0, 3))
+print(my_increment(8,3))
