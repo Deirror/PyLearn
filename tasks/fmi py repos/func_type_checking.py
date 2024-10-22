d3a234cc61514619fdd127069586fa933544d4d9
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2024-10-22 21:36:03 +0300

    Create func_type_checking.py

---
+++ tasks/fmi py repos/func_type_checking.py
@@ -0,0 +1,7 @@
+#Testing and improving skills in the field decorators in py
+
+def power(num, power):
+    return num ** power
+
+def concatenate(*strings, separator=' beep boop '):
+    return separator.join(strings)
