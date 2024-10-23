cfae9eb5c0692ec13d199fdb33fdf77b03cfc50a
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2024-10-23 17:42:45 +0300

    Update func_type_checking.py

--- tasks/fmi py repos/type checking/func_type_checking.py
+++ tasks/fmi py repos/type checking/func_type_checking.py
@@ -1,5 +1,3 @@
-#Testing and improving skills in the field decorators in py
-
 IOS_MESSAGE = {'in': 'input arguments', 'out': 'output values'}
 
 def try_io(io, valid_types, *args):
@@ -11,10 +9,11 @@ def try_io(io, valid_types, *args):
 def type_check(io):
     def types(*valid_types):
         def wrapper(func):
-            def power_wrapper(num, pwr):
+            def power_wrapper(*args, **kwargs):
+                variables = list(args) + list(kwargs)
                 if io == 'in':
-                    try_io(io, valid_types, num, pwr)
-                result = func(num, pwr)
+                    try_io(io, valid_types, *variables)
+                result = func(*variables)
                 if io == 'out':
                     try_io(io, valid_types, result)
                 return result
@@ -32,4 +31,4 @@ def concatenate(*strings, separator=' beep boop '):
     return separator.join(strings)
 
 #print(power(6j, 2))
-#print(concatenate(5, '6'))
+#print(concatenate(5, '6', 'a'))
