ae7a089be878a5999258cf98e787b3a7176cbe34
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2024-10-23 18:10:58 +0300

    Update lambdas.py

--- intro/lambdas.py
+++ intro/lambdas.py
@@ -1,2 +1,9 @@
 from secrets import choice
 from string import ascii_letters, digits, punctuation
+
+pass_gen = lambda x: ''.join(choice(ascii_letters + digits + punctuation) for _ in range(x))
+
+def func(*args, **kwargs):
+    return list(map(lambda x: ('', x), args)) + list(kwargs.items())
+
+print(pass_gen(100))
