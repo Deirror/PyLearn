06bc4278ee376df597833d5bd911cf02a4876056
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2024-10-23 20:16:47 +0300

    Update inheritance.py

--- oop/inheritance.py
+++ oop/inheritance.py
@@ -1,1 +1,34 @@
 
+class Animal:
+    def __init__(self, name):
+        self.name = name
+        self.is_alive = True
+
+    def eat(self):
+        print(f'{self.name} eats')
+
+    def sleep(self):
+        print(f'{self.name} sleeps')
+
+class Prey(Animal):
+    pass
+
+class Predator(Animal):
+    pass
+
+class Dog(Prey, Predator):
+    def __init__(self, name):
+        super().__init__(name) # !!!!
+        # or Animal.__init__(self, name) also valid
+
+    def speek(self):
+        print(f'WOOFFFFFFFFFFFFFFFFFFFFFFFFHH')
+
+dog = Dog('VulkEdinak1')
+animal = Animal('VulkEdinak69')
+
+dog.eat()
+dog.sleep()
+
+dog.speek()
+
