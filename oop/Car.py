691f040c7a7b3838a7ce9edc02524d63a7d2d6c5
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2024-10-23 18:29:38 +0300

    Create Car.py

---
+++ oop/Car.py
@@ -0,0 +1,13 @@
+class Car:
+    count_objects = 0 # static variable
+    def __init__(self, model, year, color, for_sale):
+        self.model = model
+        self.year = year
+        self.color = color
+        self.for_sale = for_sale
+        Car.count_objects++
+
+    def __str__(self):
+        return f'{self.color} {self.model}, made in {self.year} -> for sale?{'YESS' if self.for_sale else 'HECK NO'}'  
+
+ car = Car('Porsha', 1939, red, False) 
