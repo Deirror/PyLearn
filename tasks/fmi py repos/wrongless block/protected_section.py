f3cdf77c18dd6bac17ebcaa959d4a390f5aae994
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2024-11-11 21:22:52 +0200

    Update protected_section.py

--- tasks/fmi py repos/wrongless block/protected_section.py
+++ tasks/fmi py repos/wrongless block/protected_section.py
@@ -1,1 +1,29 @@
+class ProtectedSection:
+    def __init__(self, log=(), suppress=()):
+        self.log = log
+        self.suppress = suppress
+        self.exception = None
+
+    def __enter__(self):
+        self.exception = None  # reset state
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type is None:
+            return False
+
+        if exc_type in self.log:
+            self.exception = exc_value
+            return True
+        if exc_type in self.suppress:
+            return True
+        return False
+
+with ProtectedSection(log=(ZeroDivisionError, IndexError), suppress=(TypeError, ZeroDivisionError, Exception)) as err:
+    x = 1 / 0
+
+print(err.exception)
+# division by zero
+print(type(err.exception))
+# <class 'ZeroDivisionError'>
 
